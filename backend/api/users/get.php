b50bed7c5a50647348dfa14755437054b4071f45
thavath <thavathrath@email.com>
2020-01-20 23:21:17 +0700

    fixed database class for get.php

--- backend/api/users/get.php
+++ backend/api/users/get.php
@@ -1,12 +1,14 @@
 <?php 
-require_once "../../databases/database.php"; 
-require_once "../../models/user.php"; 
+include_once "../../databases/database.php"; 
+require_once "../../models/user.php";  
+$database = new Database();
+$conn = $database->getConnection(); 
 if($conn) { 
     //header("Access-Control-Allow-Origin: http://php.thavath.com:8080");
     header("Access-Control-Allow-Origin: http://php.thavath.com:8080");
     header("Access-Control-Allow-Methods: GET");
     header("Content-Type: application/json; charset=UTF-8");
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') { 
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {  
         if(isset($_GET["id"])){
             $id = $_GET["id"];
             $stmt = $conn->prepare('SELECT t.id, t.username, t.password, t.email, CONCAT(t.country_code,SUBSTRING(t.phone_number, 2, LENGTH(t.phone_number))) AS phone_number,DATE_FORMAT(t.created_at, "%d-%b-%Y %l:%i %p") created_at FROM users t WHERE t.id=:id');
